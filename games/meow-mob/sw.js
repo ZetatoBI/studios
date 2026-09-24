8cc28c83267f266f67ecd070160b45f14f806f20
Ramiimar3 <ramiaw95@gmail.com>
2026-09-24 10:51:54 -0700

    Add files via upload

--- games/meow-mob/sw.js
+++ games/meow-mob/sw.js
@@ -2,7 +2,7 @@
 const VERSION = 'meowmob-v3';
 const FILES = ['./','index.html','manifest.webmanifest',
   'fonts/fredoka-latin-400-normal.woff2','fonts/fredoka-latin-500-normal.woff2','fonts/fredoka-latin-600-normal.woff2','fonts/fredoka-latin-700-normal.woff2',
-  'icons/icon-192.png','icons/icon-512.png','icons/icon-maskable-512.png','icons/apple-touch-icon.png','icons/favicon-32.png'];
+  'icons/icon-192.png','icons/icon-512.png','icons/icon-maskable-512.png','icons/apple-touch-icon.png','icons/favicon-32.png','icons/cover.jpg'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(VERSION).then(c => c.addAll(FILES)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k.startsWith('meowmob-') && k !== VERSION).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
 // Network first for the page (so updates show up), cache first for everything else.
